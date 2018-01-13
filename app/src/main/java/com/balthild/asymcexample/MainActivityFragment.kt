5501f647dbb0b24b4aa84ece4a95807d4838d3b5
Roman Elizarov <elizarov@gmail.com>
2018-01-13 15:40:33 +0300

    Reproduces problem w/o kotlinx.coroutines

--- app/src/main/java/com/balthild/asymcexample/MainActivityFragment.kt
+++ app/src/main/java/com/balthild/asymcexample/MainActivityFragment.kt
@@ -1,12 +1,15 @@
 package com.balthild.asymcexample
 
-import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import kotlinx.coroutines.experimental.async
+import kotlin.coroutines.experimental.Continuation
+import kotlin.coroutines.experimental.CoroutineContext
+import kotlin.coroutines.experimental.EmptyCoroutineContext
+import kotlin.coroutines.experimental.startCoroutine
 
 /**
  * A placeholder fragment containing a simple view.
@@ -22,14 +25,19 @@ class MainActivityFragment : Fragment() {
 
         println("Some log")
 
-        async {
-            println("Async log")
-
-            activity.runOnUiThread {
-                tv.text = "Async message"
-            }
+        invokeSuspendWithoutDispatcher {
+            println("Suspend log")
+            tv.text = "Suspend message"
         }
 
         return layout
+    }
+
+    private fun invokeSuspendWithoutDispatcher(block: suspend () -> Unit) {
+        block.startCoroutine(object: Continuation<Unit> {
+            override val context: CoroutineContext get() = EmptyCoroutineContext
+            override fun resume(value: Unit) {}
+            override fun resumeWithException(exception: Throwable) {}
+        })
     }
 }
